36af42ddaacf3be81ef3cfeb9aa2a702f2adc406
Pete Royce Saldanha <159945565+peteroyce@users.noreply.github.com>
2025-09-17 16:20:41 +0530

    add batch processing

--- tests/test_server.py
+++ tests/test_server.py
@@ -334,3 +334,8 @@ class TestFmtContributorStats:
 def process_4(items):
     """Process batch."""
     return [x for x in items if x]
+
+
+def process_10(items):
+    """Process batch."""
+    return [x for x in items if x]
